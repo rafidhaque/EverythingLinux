4d88697e1eac3528c7ee3842284d5d0531cd0a1b
rafidhaque <rafhaq4@gmail.com>
2021-07-13 02:27:46 +0600

    first

---
+++ start.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt remove firefox
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+sudo wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+
+sudo apt install qbittorrent
+sudo apt install vlc
